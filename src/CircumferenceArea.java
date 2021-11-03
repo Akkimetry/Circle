19080302554733f5a45a99ec405a06c217c68c87
Akkimetry <aakash.sharma1590@gmail.com>
2021-11-03 12:10:36 -0400

    New project of Circle

---
+++ src/CircumferenceArea.java
@@ -0,0 +1,14 @@
+public class CircumferenceArea {
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle();
+
+        circle1.setR(2);
+        circle1.setX(2);
+        circle1.setY(2);
+
+        System.out.println(circle1.circumference());
+    }
+
+}
